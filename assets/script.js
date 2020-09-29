7d4085b549ea3a5a7c3e97035924fac5e1e98883
frillic <69489498+crowe828@users.noreply.github.com>
2020-09-28 21:49:58 -0400

    final changes. please use all of this

--- assets/script.js
+++ assets/script.js
@@ -1,59 +1,30 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+// <!-- USE EVERYTHING BETWEEN THESE -->
+// JavaScript for Unsplash
+$(document).ready(function () {
+  $("#search-btn").on("click", function (event) {
+    event.preventDefault();
+    // What the user searches for
+    var searchInput = $("#search-input").val().trim();
+    // What the user searches for plus the word tattoo, so the search is always related to tattoos
+    var searchPlusTattoos = searchInput + " tattoo";
+    unsplash(searchPlusTattoos);
+  });
 
-    <!-- Bulma.io Framework -->
-    <link
-      rel="stylesheet"
-      href="https://cdn.jsdelivr.net/npm/bulma@0.9.0/css/bulma.min.css"
-    />
-
-    <!-- Font Awesome  -->
-    <script
-      src="https://kit.fontawesome.com/f4677e3cb7.js"
-      crossorigin="anonymous"
-    ></script>
-
-
-    <!-- Custom Style Sheet  -->
-    <link rel="stylesheet" href="assets/style.css" />
-    <title>Inkmap</title>
-  </head>
-
-  <body>
-    <!-- Splash Page -->
-    <div class="hero-image">
-      <!-- InkMap logo -->
-      <img class="whiteblot" src="assets/images/whitblot.png" alt="ink blot" />
-      <a href="https://fontmeme.com/fonts/tattoo-ink-font/">
-        <img
-          src="https://fontmeme.com/permalink/200927/703c0397a10b2ae4e16acb5cb6be3e9a.png"
-          alt="tattoo-ink-font"
-      /></a>
-      <a href="https://fontmeme.com/sans-serif-fonts/">
-        <img
-          src="https://fontmeme.com/permalink/200927/dda6becbcb5255a19e5d0dc21e4ddd25.png"
-          alt="sans-serif-fonts"
-      /></a>
-      <!-- Button to take user to the search page -->
-      <div id="searchLink">
-        <a href="search.html" class="search-link"
-          ><button class="button is-large is-fullwidth is-light is-rounded">
-            Find Your Inspiration
-          </button></a
-        >
-      </div>
-    </div>
-
-    <!-- jQuery -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-
-    <!-- our js  -->
-    <script src="assets/script.js"></script>
-  </body>
-</html>
+  function unsplash(search) {
+    // Unsplash API
+    var unsplashKey = "3PslmuoCEMvFzIRfkyw7ksNQYvOjj8xdMYogY2RSBnY";
+    var unsplashURL =
+      "https://api.unsplash.com/search/photos/?query=" +
+      search +
+      "&client_id=" +
+      unsplashKey;
+    // Retrieving Unsplash API info
+    $.ajax({
+      url: unsplashURL,
+      method: "GET",
+    }).then(function (response) {
+      var mainContainer = document.querySelector(".main-container");
+      var imageResults = response.results;
 
       // Display every image generated from the search
       for (let i = 0; i < imageResults.length; i++) {
@@ -66,4 +37,4 @@
     });
   }
 });
-
+// <!-- USE EVERYTHING BETWEEN THESE -->
